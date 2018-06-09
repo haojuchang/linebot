c4cf870cd7fb61ba79a02dad6fcb78b54b02d0d5
haoju <black1120rock@gmail.com>
2018-06-09 12:55:28 +0800

    tet

--- app.py
+++ app.py
@@ -38,15 +38,10 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text="FOSS 說: %s" % event.message.text)
+    message = TextSendMessage(text="FOSS 說: %s" % "hi")
     line_bot_api.reply_message(
         event.reply_token,
         message)
-	temp = ["寶貝你最漂亮了", "you are so beautiful", "妳是天使", "你的腿兒好修長呀", "來來來~我帶妳出去玩", "我不會再睡啦><", "想你抱抱呀", "你最可愛了", "我愛妳呀"]
-    str = ""
-	for t in temp:
-		str = str + t
-	TextSendMessage(str)
 
 import os
 if __name__ == "__main__":
